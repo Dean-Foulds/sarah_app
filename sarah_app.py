9b0eb7ee4be7cae337e531656c29f64511675d69
Dean <dean@MSI.broadband>
2023-09-17 17:51:07 +0100

    firebase added

--- sarah_app.py
+++ sarah_app.py
@@ -9,6 +9,28 @@ with open('config.yaml') as file:
 
 hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
 
+from google.cloud import firestore
+
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("firestore.json")
+
+# Now let's make a reference to ALL of the posts
+posts_ref = db.collection("posts")
+
+# For a reference to a collection, we use .stream() instead of .get()
+for doc in posts_ref.stream():
+	st.write("The id is: ", doc.id)
+	st.write("The contents are: ", doc.to_dict())
+
+# This time, we're creating a NEW post reference for Apple
+doc_ref = db.collection("posts").document("Apple")
+
+# And then uploading some data to that reference
+doc_ref.set({
+	"title": "Apple",
+	"url": "www.apple.com"
+})
+
 authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
