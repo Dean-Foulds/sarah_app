f554c8a79831913f76bd6f5eeb5c482111d0b99d
Dean <dean@MSI.broadband>
2023-09-18 20:48:19 +0100

    pages changed and Leo blank added

--- sarah_app.py
+++ sarah_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import streamlit_authenticator as stauth
 from google.cloud import firestore
 from google.oauth2 import service_account
+import pyrebase
 
 
 
@@ -11,7 +12,7 @@ key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="sarah-app-2023")
 
-st.sidebar.success("Select a demo above.")
+st.sidebar.success("^^ pages will be added added here ^^.")
 
 # Now let's make a reference to ALL of the posts
 posts_ref = db.collection("posts")
@@ -26,9 +27,24 @@ doc_ref = db.collection("posts").document("Apple")
 
 # And then uploading some data to that reference
 doc_ref.set({
-	"title": "Apple",
-	"url": "www.apple.com"
+	"title": "App video",
+	"url": "Leo Cat"
 })
+
+
+firebase_config = {
+apiKey: "AIzaSyACe6cZbHMXQC2W3N1biiBqrtm0U4V-O0k",
+authDomain: "sarah-app-2023.firebaseapp.com",
+projectId: "sarah-app-2023",
+storageBucket: "sarah-app-2023.appspot.com",
+messagingSenderId: "836429532416",
+appId: "1:836429532416:web:b81fc9d093a43e8caaf7e5",
+measurementId: "G-HT0HG024LP"
+}
+
+firebase=pyrebase.initilize_app(firebase_config)
+
+storage=firebase.storage()
 
 
 
