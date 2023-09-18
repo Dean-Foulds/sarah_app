bce9a29f22e97537892e42d0b131c4587694ee11
Dean <dean@MSI.broadband>
2023-09-18 21:00:06 +0100

    pages changed and Leo blank added

--- sarah_app.py
+++ sarah_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import streamlit_authenticator as stauth
 from google.cloud import firestore
 from google.oauth2 import service_account
-# import pyrebase
 
 
 
@@ -32,19 +31,7 @@ doc_ref.set({
 })
 
 
-# firebase_config = {
-# apiKey: "AIzaSyACe6cZbHMXQC2W3N1biiBqrtm0U4V-O0k",
-# authDomain: "sarah-app-2023.firebaseapp.com",
-# projectId: "sarah-app-2023",
-# storageBucket: "sarah-app-2023.appspot.com",
-# messagingSenderId: "836429532416",
-# appId: "1:836429532416:web:b81fc9d093a43e8caaf7e5",
-# measurementId: "G-HT0HG024LP"
-# }
 
-# firebase=pyrebase.initilize_app(firebase_config)
-
-# storage=firebase.storage()
 
 
 
